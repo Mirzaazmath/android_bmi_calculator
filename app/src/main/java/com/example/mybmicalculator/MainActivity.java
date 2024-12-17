71411a8ae2d3984e698954e90ab3f62e1a38bb76
Mirzaazmath <azmath9603399270@gmail.com>
2024-12-17 12:42:32 +0530

    Enhance UI With Dialog

--- app/src/main/java/com/example/mybmicalculator/MainActivity.java
+++ app/src/main/java/com/example/mybmicalculator/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mybmicalculator;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
         submitBtn = findViewById(R.id.submitBtn);
 
        submitBtn.setOnClickListener((v)->{
+           // Here we are creating  a variable to store our dialog
+           Dialog resultDialog=new Dialog(this);
+           resultDialog.setContentView(R.layout.custom_dailog_layout);
+           TextView title, context;
+           Button closeBtn, okBtn;
+           LinearLayout header;
+
+           title = resultDialog.findViewById(R.id.dialogTitle);
+           context = resultDialog.findViewById(R.id.dialogContext);
+           closeBtn = resultDialog.findViewById(R.id.dialogClose);
+           okBtn = resultDialog.findViewById(R.id.dialogOk);
+           header = resultDialog.findViewById(R.id.dialogHeadBg);
            // Here we are getting
            int ft= Integer.parseInt(heightFT.getText().toString());
            int in= Integer.parseInt(heightIn.getText().toString());
@@ -39,15 +56,36 @@ public class MainActivity extends AppCompatActivity {
 
            if(totalBMI>25){
                System.out.println("OverWeight");
+               title.setText("OverWeight");
+               context.setText("You have to lose sum weight");
+               header.setBackgroundColor(getColor(R.color.red));
+
                // OverWeight
            }else if(totalBMI<18){
                // UnderWeight
                System.out.println("UnderWeight");
+               title.setText("UnderWeight");
+               context.setText("You have to gain sum weight");
+               header.setBackgroundColor(getColor(R.color.yellow));
 
            }else {
                // Healthy
                System.out.println("Healthy");
+               title.setText("Healthy");
+               context.setText("You have perfect weight");
+               header.setBackgroundColor(getColor(R.color.green));
            }
+           resultDialog.show();
+           closeBtn.setOnClickListener((v1 -> {
+               resultDialog.dismiss();
+              Toast.makeText(getApplicationContext(),"You have Pressed Close Button",Toast.LENGTH_LONG).show();
+
+           }));
+           okBtn.setOnClickListener((v1 -> {
+               resultDialog.dismiss();
+               Toast.makeText(getApplicationContext(),"You have Pressed Okay Button",Toast.LENGTH_LONG).show();
+
+           }));
 
        });
 
